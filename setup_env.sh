b9571e1fb43aea132c24cdbb76e4d912c86dc4df
Thomas Ruschival <thomas@ruschival.de>
2020-12-09 14:55:07 +0100

    add some missing packages to setup_env
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- setup_env.sh
+++ setup_env.sh
@@ -3,33 +3,33 @@
 # set -x
 # set -e
 
-
 sudo apt install -y wget gnupg2 gnupg-agent \
      dirmngr cryptsetup scdaemon pcscd secure-delete \
      hopenpgp-tools yubikey-personalization \
-     udiskie i3lock xautolock imagemagick x11-utils xdg-utils\
-     git-flow zsh
+     i3 i3-wm dex dmenu feh pulseaudio-utils i3lock xautolock \
+     imagemagick x11-utils udiskie xdg-utils \
+     git-flow zsh 
 
 ##
 # Create a link and ask if link exists and call was not forced
 ##
-function create_link(){
-	local target=$1
-	local link=$2
+function create_link() {
+    local target=$1
+    local link=$2
 
-	echo "LINK: $2 --> $1 "
+    echo "LINK: $2 --> $1 "
 
-	if [ -e $link ] && [ -z $FORCE_LINK ] ;
+    if [ -e $link ] && [ -z $FORCE_LINK ] ;
+    then
+	read -p "File $link exists, overwrite? [N|y] " -n 1 -r
+	echo # new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
-		read -p "File $link exists, overwrite? [N|y] " -n 1 -r
-		echo # new line
-		if [[ $REPLY =~ ^[Yy]$ ]]
-		then
-			ln -sf $target $link
-		fi
-	else
-		ln -s $FORCE_LINK $target $link
+	    ln -sf $target $link
 	fi
+    else
+	ln -s $FORCE_LINK $target $link
+    fi
 }
 
 ################################################################################
@@ -39,24 +39,25 @@ function create_link(){
 # work environment
 ##
 function setup_dot_links(){
-	local src_dir=$1
-	local target_dir=$2
+    local src_dir=$1
+    local target_dir=$2
 
-	for file in $(find $src_dir -maxdepth 1 -type f -name dot\* )
-	do
-		local dotfile=$(basename $file)
-		local link_name=${dotfile/dot/}
+    for file in $(find $src_dir -maxdepth 1 -type f -name dot\* )
+    do
+	local dotfile=$(basename $file)
+	local link_name=${dotfile/dot/}
 
-		if [ ! -z $WS_OVERRIDE ] && [ -e $WS_OVERRIDE/$dotfile ]
-		then
-			echo "Using override $WS_OVERRIDE/$dotfile for $file"
-			local src_root=$(realpath $WS_OVERRIDE)
-		else
-			local src_root=$(realpath $src_dir)
-		fi
-		create_link ${src_root}/${dotfile}  $target_dir/$link_name
-	done
+	if [ ! -z $WS_OVERRIDE ] && [ -e $WS_OVERRIDE/$dotfile ]
+	then
+	    echo "Using override $WS_OVERRIDE/$dotfile for $file"
+	    local src_root=$(realpath $WS_OVERRIDE)
+	else
+	    local src_root=$(realpath $src_dir)
+	fi
+	create_link ${src_root}/${dotfile} $target_dir/$link_name
+    done
 }
+
 ################################################################################
 
 ##
@@ -64,34 +65,36 @@ function setup_dot_links(){
 # Setup target directory structure and create link
 ##
 function setup_links_in_subdir(){
-	local src_dir=$1
-	local target_dir=$2
+    local src_dir=$1
+    local target_dir=$2
 
-	mkdir -p $target_dir
+    mkdir -p $target_dir
 
-	# only take subdirectories, not $src_dir
-	for file in $(find $src_dir -mindepth 1 -type f )
-	do
-		local confdir_name=$(dirname $file)
-		local link_name=${confdir_name/dot/}
+    # only take subdirectories, not $src_dir
+    for file in $(find $src_dir -mindepth 1 -type f )
+    do
+	local confdir_name=$(dirname $file)
+	local link_name=${confdir_name/dot/}
 
-		if [ ! -z $WS_OVERRIDE ] && [ -e $WS_OVERRIDE/$confdir_name ]
-		then
-			echo "Using override $WS_OVERRIDE/$confdir for $confdir"
-			local src_root=$(realpath $WS_OVERRIDE)
-		else
-			local src_root=$(realpath $confdir_name)
-		fi
+	if [ ! -z $WS_OVERRIDE ] && [ -e $WS_OVERRIDE/$confdir_name ]
+	then
+	    echo "Using override $WS_OVERRIDE/$confdir for $confdir"
+	    local src_root=$(realpath $WS_OVERRIDE)
+	else
+	    local src_root=$(realpath $confdir_name)
+	fi
 
-		create_link ${src_root}  $target_dir/$link_name
-	done
+	create_link ${src_root}  $target_dir/$link_name
+    done
 }
 
 ################################################################################
 
 function gnupg_setup(){
+
     mkdir -p $HOME/.config/systemd/user/
     cp /usr/share/doc/gnupg/examples/systemd-user/*  $HOME/.config/systemd/user/
+
     # use the ssh-agent enabled gpg-agent.service file
     cp $SCRIPT_DIR/dot.config/gpg-agent.service $HOME/.config/systemd/user/
     # gnupg - copy conf-files ! no not link
@@ -103,22 +106,22 @@ function gnupg_setup(){
     systemctl --user enable dirmngr.socket
 }
 
-
 ################################################################################
 function scale_lockscreen(){
-	screensize=$(xdpyinfo | awk '/dimensions/{print $2}')
-	convert -scale $screensize .wallpapers/lockscreen.png \
-		   .wallpapers/lockscreen-scaled.png
+    screensize=$(xdpyinfo | awk '/dimensions/{print $2}')
+    convert -scale $screensize .wallpapers/lockscreen.png \
+	    .wallpapers/lockscreen-scaled.png
 
 }
 
 ################################################################################
 function i3_conf_fixup(){
-	echo fixup
+    echo fixup
 }
 
+################################################################################
 function print_help() {
-	cat <<EOF
+    cat <<EOF
 $0 creates symbolic links in \$HOME and respective sub directories to
 files provided by this repository.
 For this to work this script expects the configuration files to be in the
@@ -142,23 +145,26 @@ EOF
 }
 
 
+################################################################################
+
 while getopts w:hf option
 do
-	case "${option}"
-	in
-		w)
-			WS_OVERRIDE=${OPTARG}
-			;;
-		f)
-			FORCE_LINK="-f"
-			;;
-		*)
-			print_help $0
-			;;
-	esac
+    case "${option}"
+    in
+	w)
+	    WS_OVERRIDE=${OPTARG}
+	    ;;
+	f)
+	    FORCE_LINK="-f"
+	    ;;
+	*)
+	    print_help $0
+	    ;;
+    esac
 done
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # install ohmyzsh https://github.com/ohmyzsh/ohmyzsh
 sh -c "$(wget -O- \
    https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -166,6 +172,7 @@ sh -c "$(wget -O- \
 # Create local overrides
 touch $HOME/.zshenv.local
 touch $HOME/.zsh_aliases.local
+
 # create config folder if not setup
 [ -d $HOME/.config ] || mkdir $HOME/.config
 
