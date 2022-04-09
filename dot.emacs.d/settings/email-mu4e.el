b6035a00e2157e171ac04c24153303f3291644e0
Thomas Ruschival <thomas@ruschival.de>
2022-04-09 13:02:09 +0200

    Emacs: pimp mu4e
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- dot.emacs.d/settings/email-mu4e.el
+++ dot.emacs.d/settings/email-mu4e.el
@@ -13,6 +13,7 @@
 	  '((:maildir "/ruschival.de/INBOX"   :key ?i)
 	    (:maildir "/ruschival.de/Sent"    :key ?s)
 	    (:maildir "/ruschival.de/Drafts"  :key ?d)
+	    (:maildir "/ruschival.de/Shopping"   :key ?h)
 	    (:maildir "/ruschival.de/Trash"   :key ?t)
 	    ))
 
@@ -49,16 +50,15 @@
    message-citation-line-format "On %Y-%m-%d at %R %Z, %f wrote:\n"
    )
 
-  
   ;; Use gnus as mail reader, capable of decoding inline PGP
   ;;(setq mu4e-view-use-gnus t)
   ;; Some magic to show html mail
   (setq mu4e-html2text-command "html2text -utf8 -width 72")
   (setq mu4e-view-html-plaintext-ratio-heuristic most-positive-fixnum)
-  (setq shr-color-visible-luminance-min 80)
+  (setq shr-color-visible-luminance-min 60)
+  (setq shr-use-colors t)
 
-(add-hook 'mu4e-view-mode-hook
-	    (
+  (add-hook 'mu4e-view-mode-hook(
 	     lambda ()
 	     (message "mu4e-view-mode-hook" )
 	     ;; no trailing whitespace highlight for messages
@@ -74,7 +74,10 @@
   (add-hook 'mu4e-headers-mode-hook (lambda ()
 				      (display-line-numbers-mode -1)))
 
+  (setq mu4e-compose-crypto-reply-plain-policy 'sign)
   ;; (add-hook 'message-send-hook 'mml-secure-message-sign-pgpmime)
+  (setq mml-secure-openpgp-encrypt-to-self t)
+  (setq mml-secure-openpgp-sign-with-sender t)
 
   ;; the headers to show in the headers list -- a pair of a field
   ;; and its width, with `nil' meaning 'unlimited'
@@ -86,6 +89,7 @@
 	   (:size          .   6)
 	   (:from-or-to    .  22)
 	   (:subject       .  nil))) ;; alternatively, use :thread-subject
+   (setq mu4e-headers-date-format "%+4Y-%m-%d")
 
   ;; program to get mail; alternatives are 'fetchmail', 'getmail'
   ;; isync or your own shellscript. called when 'U' is pressed in
@@ -100,6 +104,38 @@
   (setq mu4e-compose-reply-to-address "thomas@ruschival.de"
 	user-mail-address "thomas@ruschival.de"
 	user-full-name  "Thomas Ruschival")
+
+  (defun ok/message-attachment-present-p ()
+    "Return t if a non-gpg attachment is found in the current message."
+    (save-excursion
+      (save-restriction
+	(widen)
+	(goto-char (point-min))
+	(when (search-forward "<#part type" nil t) t))))
+
+  (setq ok/message-attachment-regexp
+	(regexp-opt '("[Ww]e send"
+                      "[Ii] send"
+                      "attach"
+                      "[aA]ngehängt"
+                      "[aA]nhang"
+                      "[sS]chicke"
+                      "angehaengt"
+                      "haenge"
+                      "hänge")))
+
+  (defun ok/message-warn-if-no-attachments ()
+    "Check if there is an attachment in the message if I claim it."
+    (when (and (save-excursion
+		 (save-restriction
+		   (widen)
+		   (goto-char (point-min))
+		   (re-search-forward ok/message-attachment-regexp nil t)))
+	       (not (ok/message-attachment-present-p)))
+      (unless (y-or-n-p "No attachment. Send the message?")
+	(keyboard-quit))))
+
+  (add-hook 'message-send-hook #'ok/message-warn-if-no-attachments)
 
   ;; smtp mail setting
   (setq
